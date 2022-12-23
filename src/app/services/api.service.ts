35b8de7c91498966e5ca35f0f7cacffedb484653
Anandadhamkavil <anandadhamkavil123@gmail.com>
2022-12-23 11:37:30 +0530

    contact app v4

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -42,4 +42,9 @@ export class ApiService {
   deleteContact(contactId:any){
    return this.http.delete(`${this.baseUrl}/${contactId}`)
   }
+
+   // update Contact detail according to user select  
+   updateContact(contactId:any,contactBody:any){
+     return this.http.put(`${this.baseUrl}/${contactId}`,contactBody)
+  }
 }
